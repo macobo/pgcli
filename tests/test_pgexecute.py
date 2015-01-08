f51902e2c66500d93d290c350ed774370da7dbc6
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2015-01-08 12:24:32 +0200

    Tests checking the errors returned on invalid syntax/commands

--- tests/test_pgexecute.py
+++ tests/test_pgexecute.py
@@ -1,4 +1,6 @@
 # coding=UTF-8
+import pytest
+import psycopg2
 from pgcli.pgexecute import _parse_dsn
 from textwrap import dedent
 from utils import *
@@ -53,6 +55,17 @@ def test_conn(executor):
         +-----+
         SELECT 1""")
 
+@dbtest
+def test_invalid_syntax(executor):
+    with pytest.raises(psycopg2.ProgrammingError) as excinfo:
+        run(executor, 'invalid syntax!')
+    assert 'syntax error at or near "invalid"' in str(excinfo.value)
+
+@dbtest
+def test_invalid_column_name(executor):
+    with pytest.raises(psycopg2.ProgrammingError) as excinfo:
+        run(executor, 'select invalid command')
+    assert 'column "invalid" does not exist' in str(excinfo.value)
 
 @dbtest
 def test_unicode_support_in_output(executor):
